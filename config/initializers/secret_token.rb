9f6b97ff3a5df763976edfb492b18c10cd24fe8a
Mohnish Thallavajhula <arrowgunz@gmail.com>
2011-10-24 12:42:12 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CpypstMe::Application.config.secret_token = '201d2d37bd2e0a3d9b2a31f96713d66cc70526ebea43b7da5861c9b03e8bcff12078d0f078dff22db28442b31956ca8294d3b56c5ea602597a3257ba2213e583'
